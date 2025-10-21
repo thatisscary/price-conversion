84dae21c80be8e8061aa325b1d6a0cf010b0a09d
Matthew Snow <matthew_snow@hotmail.com>
2025-10-21 14:04:13 -0700

    Interim Commit. Refactored and improved Currency Conversion project -- adding missing files

--- currency-conversion-api/Contracts/External/FiscalData.cs
+++ currency-conversion-api/Contracts/External/FiscalData.cs
@@ -4,14 +4,13 @@
         where TContaintedType : class, IForeignCurrencyItem
     {
         public FiscalData()
-        { 
+        {
             data = Array.Empty<TContaintedType>();
             meta = new Meta();
         }
 
         public TContaintedType[] data { get; set; }
         public Meta meta { get; set; }
-        
     }
 
     public class Meta
